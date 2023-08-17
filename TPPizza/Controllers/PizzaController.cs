8194d57df1faa5d1ae35253b277e4a3c78917c06
gaelchevalier <89252419+gaelchevalier@users.noreply.github.com>
2023-08-17 15:51:40 +0200

    afficher liste pizzas

--- TPPizza/Controllers/PizzaController.cs
+++ TPPizza/Controllers/PizzaController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using BO;
 
 namespace TPPizza.Controllers
 {
@@ -8,7 +9,8 @@ namespace TPPizza.Controllers
         // GET: PizzaController
         public ActionResult Index()
         {
-            return View();
+            IList<Pizza> pizzas = PizzasMock;
+            return View(pizzas);
         }
 
         // GET: PizzaController/Details/5
@@ -79,5 +81,44 @@ namespace TPPizza.Controllers
                 return View();
             }
         }
+
+        // A mettre dans le controlleur pizza
+
+        public static List<Ingredient> IngredientsDisponibles => new List<Ingredient> {
+                    new Ingredient{Id=1,Nom="Mozzarella"},
+                    new Ingredient{Id=2,Nom="Jambon"},
+                    new Ingredient{Id=3,Nom="Tomate"},
+                    new Ingredient{Id=4,Nom="Oignon"},
+                    new Ingredient{Id=5,Nom="Cheddar"},
+                    new Ingredient{Id=6,Nom="Saumon"},
+                    new Ingredient{Id=7,Nom="Champignon"},
+                    new Ingredient{Id=8,Nom="Poulet"}
+                };
+
+        public static List<Pate> PatesDisponibles => new List<Pate> {
+                    new Pate{ Id=1,Nom="Pate fine, base crême"},
+                    new Pate{ Id=2,Nom="Pate fine, base tomate"},
+                    new Pate{ Id=3,Nom="Pate épaisse, base crême"},
+                    new Pate{ Id=4,Nom="Pate épaisse, base tomate"}
+                };
+
+        public static List<Pizza> PizzasMock => new List<Pizza>
+        {
+            new Pizza{
+                    Id=1, 
+                    Nom="Margarita", 
+                    Pate=PatesDisponibles.Single(x => x.Id==1), 
+                    Ingredients=new List<Ingredient>{ IngredientsDisponibles.Single(x => x.Id==1) }
+            },
+            new Pizza{
+                    Id=2,
+                    Nom="Champi",
+                    Pate=PatesDisponibles.Single(x => x.Id==2),
+                    Ingredients=new List<Ingredient>{ 
+                        IngredientsDisponibles.Single(x => x.Id==1),
+                        IngredientsDisponibles.Single(x => x.Id==7)
+                    }
+            },
+        };
     }
 }
